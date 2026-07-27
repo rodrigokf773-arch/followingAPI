c106e61b4b4239b715969d8823a1e4fa722157b8
rodrigokf773-arch <rodrigokf773@gmail.com>
2026-07-27 17:41:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -224,8 +224,6 @@ app.get("/is-following", authenticateRequest, async (req, res) => {
     }
 });
 
-const PORT = Number(process.env.PORT) || 3000;
-
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`API iniciada na porta ${PORT}.`);
 });
